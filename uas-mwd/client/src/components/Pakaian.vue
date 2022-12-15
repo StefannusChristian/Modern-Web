5f952a8b387ea663a351283bbf3a9b4554d8cf17
samuelms21 <ssamuelmarcellino@gmail.com>
2022-12-15 17:03:10 +0700

    Add initial routes for product category in flask

---
+++ uas-mwd/client/src/components/Pakaian.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <li v-for="pakai in pakaian" :key="pakai.product_id" :id="pakai.product_id">
+      {{ pakai }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Pakaian",
+  data() {
+    return {
+      pakaian: null,
+    };
+  },
+  methods: {
+    getPakaian() {
+      const path = "http://127.0.0.1:5000/pakaian";
+      axios
+        .get(path)
+        .then((res) => {
+          this.pakaian = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getPakaian();
+  },
+};
+</script>
+
+<style></style>
