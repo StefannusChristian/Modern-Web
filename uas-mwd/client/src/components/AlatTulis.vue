6b2e569c52d2d31f3c795b25c53a00a4421dd64f
samuelms21 <ssamuelmarcellino@gmail.com>
2022-12-15 20:06:25 +0700

    Add UI components such as navigation links, buttons, and product cards

--- uas-mwd/client/src/components/AlatTulis.vue
+++ uas-mwd/client/src/components/AlatTulis.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>Ini Component Alat Tulis</h1>
-  <ul>
-    <li v-for="alat in alat_tulis" :key="alat.product_id" :id="alat.product_id">
-      {{ alat }}
-    </li>
-  </ul>
+  <h1 class="mb-3">Ini Component Alat Tulis</h1>
+  <div class="d-flex row flex-wrap justify-content-center">
+    <div
+      class="card product-card mx-2"
+      v-for="alat in alat_tulis"
+      :key="alat.product_id"
+      :id="alat.product_id"
+    >
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ alat.product_name }}
+        </h5>
+        <h6 class="card-subtitle mb-2">IDR {{ alat.product_price }}</h6>
+        <button class="btn btn-dark d-block w-100">
+          <i class="bi bi-cart-plus-fill me-2"></i>Add Product
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
---
+++ uas-mwd/client/src/components/LeftSidebar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container w-25">
+    <div class="p-3 bg-light">
+      <div class="mb-3">
+        <h3>Invoice Header</h3>
+        <div class="bg-dark text-white p-2 m-2 rounded-3">
+          Admin:<br />
+          <span class="fw-semibold">John Doe</span>
+        </div>
+      </div>
+      <div class="mb-3">
+        <h4>Invoice Body</h4>
+        <ul class="list-group">
+          <li class="list-group-item">Item 1</li>
+          <li class="list-group-item">Item 2</li>
+          <li class="list-group-item">Item 3</li>
+        </ul>
+      </div>
+      <button class="btn btn-success d-block w-100 mb-1">
+        <i class="bi bi-credit-card-2-back-fill me-2"></i>Pay
+      </button>
+      <button class="btn btn-danger d-block w-100">
+        <i class="bi bi-x-square-fill me-2"></i> Cancel
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeftSidebar",
+};
+</script>
