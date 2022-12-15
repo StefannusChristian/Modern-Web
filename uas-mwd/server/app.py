bdcbfa8cebfebfb8c9b8fad214cf72d5e687187e
Stefannus Christian <schristian38@students.calvin.ac.id>
2022-12-15 17:05:39 +0700

    biar bisa merge

--- uas-mwd/server/app.py
+++ uas-mwd/server/app.py
@@ -1,6 +1,5 @@
 from package import app, db
 from package.models import User, Invoice, InvoiceDetail, Category, Product
-import socket
 
 @app.shell_context_processor
 def make_shell_context(): return {
@@ -11,8 +10,3 @@ def make_shell_context(): return {
     'Category': Category,
     'Product': Product
     }
-
-if __name__ == '__main__':
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-    app.run(host=ip_address, port=5000, debug=True, threaded=False)
