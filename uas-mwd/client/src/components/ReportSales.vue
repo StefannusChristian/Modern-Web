1752b4f6053409281fc7e221f5f67e35eafd7be1
samuelms21 <ssamuelmarcellino@gmail.com>
2022-12-23 02:33:58 +0700

    Finish report sales layout and design

--- uas-mwd/client/src/components/ReportSales.vue
+++ uas-mwd/client/src/components/ReportSales.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-100 mb-5 container py-3">
-    <h2 class="mb-3">Report Sales</h2>
+    <h2 class="mb-3 fw-800 fs-1">Report Sales</h2>
     <div
       class="mb-3 d-flex justify-content-between align-items-center container"
     >
@@ -13,25 +13,141 @@
         }}</span>
       </div>
     </div>
-    <ul class="list-group">
-      <li
-        v-for="invoice in invoices"
-        :key="invoice.invoiceId"
-        :id="invoice.invoiceId"
-        class="list-group-item"
-      >
-        InvoiceID: {{ invoice.invoiceId }}<br />
-        <ul>
-          <li
-            v-for="invoiceDetail in invoice.details"
-            :key="invoiceDetail.invoice_detail_id"
-          >
-            Product: {{ invoiceDetail.product_name }} ({{ invoiceDetail.qty }})
-          </li>
-        </ul>
-        Total: {{ invoice.totalPrice }}
-      </li>
-    </ul>
+    <div class="d-flex justify-content-between align-items-start">
+      <div class="w-50 container px-2 d-flex flex-wrap">
+        <div
+          v-for="invoice in invoices"
+          :key="invoice.invoiceId"
+          :id="invoice.invoiceId"
+          class="p-3 rounded-3 bg-light mb-2 mx-1"
+          style="width: 48%"
+        >
+          <div class="mb-2">InvoiceID: {{ invoice.invoiceId }}</div>
+          <ul class="list-group mb-3">
+            <li
+              v-for="invoiceDetail in invoice.details"
+              :key="invoiceDetail.invoice_detail_id"
+              class="list-group-item d-flex justify-content-between"
+            >
+              <div>{{ invoiceDetail.product_name }}</div>
+              <div>x{{ invoiceDetail.qty }}</div>
+            </li>
+          </ul>
+          Total: <br /><span class="fw-600">{{ invoice.totalPrice }}</span>
+        </div>
+      </div>
+      <div class="w-50 container px-3">
+        <h3 class="mb-3">Sales Per Category</h3>
+        <div class="my-3">
+          <h5>Pakaian</h5>
+          <table class="table table-striped">
+            <thead>
+              <tr class="table-dark">
+                <th>Product Name</th>
+                <th>Quantity</th>
+                <th>Sales</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in categorySales.Pakaian"
+                :key="item.product_name"
+              >
+                <td class="w-25">{{ item.product_name }}</td>
+                <td>{{ item.sum_qty }}</td>
+                <td>
+                  {{
+                    new Intl.NumberFormat("id-ID", {
+                      style: "currency",
+                      currency: "IDR",
+                    })
+                      .format(parseInt(item.sum_qty) * item.product_price)
+                      .slice(0, -3)
+                  }}
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Total Sales</td>
+                <td>Rp 1.000.000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="my-4">
+          <h5>Makanan</h5>
+          <table class="table table-striped">
+            <thead>
+              <tr class="table-dark">
+                <th>Product Name</th>
+                <th>Quantity</th>
+                <th>Sales</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in categorySales.Makanan"
+                :key="item.product_name"
+              >
+                <td class="w-25">{{ item.product_name }}</td>
+                <td>{{ item.sum_qty }}</td>
+                <td>
+                  {{
+                    new Intl.NumberFormat("id-ID", {
+                      style: "currency",
+                      currency: "IDR",
+                    })
+                      .format(parseInt(item.sum_qty) * item.product_price)
+                      .slice(0, -3)
+                  }}
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Total Sales</td>
+                <td>Rp 1.000.000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="my-3">
+          <h5>Alat Tulis</h5>
+          <table class="table table-striped">
+            <thead>
+              <tr class="table-dark">
+                <th>Product Name</th>
+                <th>Quantity</th>
+                <th>Sales</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in categorySales.AlatTulis"
+                :key="item.product_name"
+              >
+                <td class="w-25">{{ item.product_name }}</td>
+                <td>{{ item.sum_qty }}</td>
+                <td>
+                  {{
+                    new Intl.NumberFormat("id-ID", {
+                      style: "currency",
+                      currency: "IDR",
+                    })
+                      .format(parseInt(item.sum_qty) * item.product_price)
+                      .slice(0, -3)
+                  }}
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Total Sales</td>
+                <td>Rp 1.000.000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -45,6 +161,7 @@ export default {
       total_user_sales: 0,
       total_sales_display: null,
       username: sessionStorage.getItem("currentLoggedIn"),
+      categorySales: { Pakaian: [], AlatTulis: [], Makanan: [] },
     };
   },
   methods: {
@@ -55,7 +172,7 @@ export default {
       axios
         .post(
           path,
-          { username: username },
+          { username: this.username },
           {
             headers: {
               "Content-Type": "application/json",
@@ -72,12 +189,12 @@ export default {
           console.error(error);
         });
     },
-    getCurrentUserId() {
-      const path = "http://127.0.0.1:5000/sales_per_category" + this.username;
+    getSalesPerCategory() {
+      const path = "http://127.0.0.1:5000/sales_per_category";
       axios
         .post(
           path,
-          { username: username },
+          { username: this.username },
           {
             headers: {
               "Content-Type": "application/json",
@@ -86,8 +203,17 @@ export default {
         )
         .then((res) => {
           // console.log(res);
-          this.processInvoiceData(res.data);
-          console.log(this.invoices);
+          let data = res.data;
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].category_id === 1) {
+              this.categorySales.Pakaian.push(data[i]);
+            } else if (data[i].category_id === 2) {
+              this.categorySales.Makanan.push(data[i]);
+            } else if (data[i].category_id === 3) {
+              this.categorySales.AlatTulis.push(data[i]);
+            }
+          }
+          console.log(this.categorySales);
         })
         .catch((error) => {
           // eslint-disable-next-line
@@ -124,6 +250,7 @@ export default {
   },
   created() {
     this.getInvoices();
+    this.getSalesPerCategory();
   },
 };
 </script>
