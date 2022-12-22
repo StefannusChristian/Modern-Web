9c447bb509e6f804065def0f538edab5b5faf79f
Stefannus Christian <schristian38@students.calvin.ac.id>
2022-12-23 04:33:19 +0700

    add form tag to login

--- uas-mwd/client/src/components/Login.vue
+++ uas-mwd/client/src/components/Login.vue
@@ -3,47 +3,49 @@
     <h1 class="mb-5 text-center fw-800">Point of Sales App</h1>
     <h3 class="fw-600 mb-4 text-center">Log In</h3>
     <div class="container w-50">
-      <div class="form-floating mb-3">
-        <input
-          type="text"
-          class="form-control"
-          id="username"
-          placeholder="admin_username"
-          v-model="username"
-          required
-        />
-        <label for="floatingInput">Username:</label>
-      </div>
-      <div class="form-floating mb-3">
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          placeholder="Password"
-          v-model="password"
-          required
-        />
-        <label for="floatingPassword">Password</label>
-      </div>
-      <button
-        class="btn btn-dark w-100 text-center mb-3"
-        @click="sendLoginData"
-      >
-        Log In
-      </button>
-      <div
-        v-if="login_error"
-        class="alert alert-danger alert-dismissible fade show"
-        role="alert"
-      >
-        <strong>Invalid credentials.</strong>
+      <form>
+        <div class="form-floating mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="username"
+            placeholder="admin_username"
+            v-model="username"
+            required
+          />
+          <label for="floatingInput">Username:</label>
+        </div>
+        <div class="form-floating mb-3">
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Password"
+            v-model="password"
+            required
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
         <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="alert"
-          aria-label="Close"
-        ></button>
-      </div>
+          class="btn btn-dark w-100 text-center mb-3"
+          @click="sendLoginData"
+        >
+          Log In
+        </button>
+        <div
+          v-if="login_error"
+          class="alert alert-danger alert-dismissible fade show"
+          role="alert"
+        >
+          <strong>Invalid credentials.</strong>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
